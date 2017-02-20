c23b92faeee58bde5fcbe52e0a23a3b6996df0ec
Nicholas Hemenway <n@nhem.co>
2017-02-20 13:45:06 -0800

    working cipher

---
+++ Cipher.rb
@@ -0,0 +1,44 @@
+def caesar_cipher phrase, shift
+  $phrase = phrase
+  $shift = shift
+
+  lower_case = ('a'..'z').to_a
+  lower_case += lower_case
+  upper_case = ('A'..'Z').to_a
+  upper_case += upper_case
+
+  translation = []
+
+  letters = phrase.split('')
+
+  letters.each do |letter|
+    if lower_case.index(letter)
+      position = lower_case.index(letter) + shift
+      letter = lower_case[position]
+    elsif upper_case.index(letter)
+      position = upper_case.index(letter) + shift
+      letter = upper_case[position]
+    end
+
+    translation.push(letter)
+  end
+
+  translation.join
+
+end
+
+# caesar_cipher("HiZ This!",2)
+# caesar_cipher("What a string!", 5)
+
+puts "What would you like to encode?"
+
+phrase = gets.chomp
+
+puts "Select an offset between 1 and 25"
+
+shift = gets.chomp.to_i
+
+puts "Encoding..."
+puts "Your encoded message..."
+
+puts caesar_cipher(phrase, shift)
